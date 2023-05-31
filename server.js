c0d3cda9b5c338df197f0cd815017563cc8f22e8
Sourav <2020ume1058@mnit.ac.in>
2023-06-01 02:22:40 +0530

    updated

--- server.js
+++ server.js
@@ -2,15 +2,7 @@ const app = require('./app');
 const PORT = process.env.PORT || 5100;
 
 
-const isBuildProcess = process.env.NOW_REGION === 'dev1';
-
-if (!isBuildProcess) {
-    const server = app.listen(PORT, () => {
-        console.log('Server started on ', PORT);
-    });
-}
-
-
-// const server = app.listen(PORT, () => {
-//     console.log('Server started on', PORT);
-// });
+const server = app.listen(() => {
+    const { port } = server.address();
+    console.log(`Server started on port ${port}`);
+});
