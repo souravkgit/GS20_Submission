b7cdda7b7bffa422d4432617dabf52ca8b76444d
Sourav <2020ume1058@mnit.ac.in>
2023-06-01 02:11:04 +0530

    updated

--- app.js
+++ app.js
@@ -13,9 +13,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5100;
 
-const server = app.listen(PORT, () => {
-    console.log('Server started on ', PORT);
-})
+// const server = app.listen(PORT, () => {
+//     console.log('Server started on ', PORT);
+// })
 
 
 
+const isBuildProcess = process.env.NOW_REGION === 'dev1';
+
+if (!isBuildProcess) {
+    const server = app.listen(PORT, () => {
+        console.log('Server started on ', PORT);
+    });
+}
+
+module.exports = app;
